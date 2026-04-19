75eda2d908af646e424783e0ad38cb046bd3a461
Mahwish <eggisgay69420@gmail.com>
2026-04-19 18:04:04 +0530

    iron golem health bar and player attack working

--- scripts/iron_golem.gd
+++ scripts/iron_golem.gd
@@ -1,11 +1,20 @@
 extends Node2D
 
+var max_health = 100
+var health = 100
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$HealthBarContainer/HealthBar.max_value = max_health
+	$HealthBarContainer/HealthBar.value = health
 
+func take_damage(amount: int) -> void:
+	health -= amount
+	health = clamp(health, 0, max_health)
+	$HealthBarContainer/HealthBar.value = health
+	print("Slime took ", amount, " damage! HP: ", health, "/", max_health)
+	if health <= 0:
+		die()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func die() -> void:
+	print("Slime died!")
+	queue_free()
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,40 +1,56 @@
 extends CharacterBody2D
 
-
 const SPEED = 120.0
 const JUMP_VELOCITY = -270.0
+
 @onready var sprite = $AnimatedSprite2D
+@onready var attack_hitbox = $AttackHitbox  # Area2D child node
 
 var attack_damage = 25
 var is_attacking = false
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Attack input
+	if Input.is_action_just_pressed("attack") and not is_attacking:
+		_do_attack()
+
 	var direction := Input.get_axis("move_left", "move_right")
-	
-	# Animations
+
 	if is_on_floor():
 		if direction != 0:
 			velocity.x = direction * SPEED
-			sprite.play("walk")
-			sprite.flip_h = direction < 0  # Flip when moving left
+			if not is_attacking:
+				sprite.play("walk")
+			sprite.flip_h = direction < 0
 		else:
 			velocity.x = 0
-			sprite.play("idle")
+			if not is_attacking:
+				sprite.play("idle")
 	else:
-		sprite.play("jump")
+		if not is_attacking:
+			sprite.play("jump")
 
 	move_and_slide()
 
+func _do_attack():
+	is_attacking = true
+	sprite.play("attack")
+
+	for area in attack_hitbox.get_overlapping_areas():
+		print("Hello")
+		var parent = area.get_parent()
+		if parent.has_method("take_damage"):
+			parent.take_damage(attack_damage)
+
+	await sprite.animation_finished
+	is_attacking = false
+	
 func _ready():
 	Global.checkpoint_position = global_position
 	await get_tree().process_frame
@@ -47,18 +63,3 @@ func _on_checkpoint(pos):
 
 func die():
 	global_position = Global.checkpoint_position
-	
-#func _do_attack():
-	#is_attacking = true
-	#sprite.play("attack")
-	#$AttackHitbox/CollisionShape2D.disabled = false
-#
-	## Deal damage to anything in hitbox right now
-	#for body in attack_hitbox.get_overlapping_bodies():
-		#if body.has_method("take_damage"):
-			#body.take_damage(attack_damage)
-#
-	## Wait for animation to finish then reset
-	#await sprite.animation_finished
-	#$AttackHitbox/CollisionShape2D.disabled = true
-	#is_attacking = false
