8fb147575b22f094a9281fa643f4a627845dbda3
sid314 <sidaa314@gmail.com>
2026-04-19 20:45:55 +0530

    level

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,8 +1,9 @@
 extends Area2D
 
-@onready var scoreLabel: Label = $"../../Overlay/Score"
-
+# We deleted the @onready variable because we don't need the exact path anymore!
 
 func _on_body_entered(body: Node2D) -> void:
-	scoreLabel.add_score(10)
+	# This tells Godot to find any node in the "score_label" group and call its "add_score" function
+	get_tree().call_group("score_label", "add_score", 10)
+	
 	queue_free()
